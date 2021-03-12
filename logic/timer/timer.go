39a7bb1368f51cac080fed5eb1a40a66fd9b7c52
Jakub Dóka <mlokogegelmlok@gmail.com>
2021-03-12 14:02:19 +0100

    adding timer package

---
+++ logic/timer/timer.go
@@ -0,0 +1,47 @@
+package timer
+
+// Timer measures a elapsed time, its very simple but effective
+type Timer struct {
+	Progress, Period float64
+}
+
+// Period returns timer with given period
+func Period(period float64) Timer {
+	return Timer{Period: period}
+}
+
+// Progress returns timer with given progress and period
+func Progress(progress float64, period float64) Timer {
+	return Timer{progress, period}
+}
+
+// Tick increases progress by delta
+func (t *Timer) Tick(delta float64) {
+	t.Progress += delta
+}
+
+// Done returns whether progress exceeded period
+func (t Timer) Done() bool {
+	return t.Progress > t.Period
+}
+
+// Reset sets progres to 0
+func (t *Timer) Reset() {
+	t.Progress = 0
+}
+
+// TickDone does Tick and Done in one step
+func (t *Timer) TickDone(delta float64) bool {
+	t.Progress += delta
+	return t.Progress > t.Period
+}
+
+// TickDoneReset does tick and if Done that it resets
+func (t *Timer) TickDoneReset(delta float64) bool {
+	t.Progress += delta
+	if t.Progress > t.Period {
+		t.Progress = 0
+		return true
+	}
+	return false
+}
